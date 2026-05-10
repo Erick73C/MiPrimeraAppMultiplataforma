f49c2c3071bf8be8a73bb6b1e3e6e02cbe609dc1
Erick Omar Perez Gonzlaez <s22120250@alumnos.itsur.edu.mx>
2026-05-09 19:18:33 -0600

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/example/miprimeraappmultiplataforma/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.miprimeraappmultiplataforma
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/jvmMain/kotlin/com/example/miprimeraappmultiplataforma/main.kt
@@ -0,0 +1,13 @@
+package com.example.miprimeraappmultiplataforma
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "MiPrimeraAppMultiplataforma",
+    ) {
+        App()
+    }
+}
